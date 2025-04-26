8b0f6ecbff3134be8c6e8f0475c588de8a23d5c4
Kevin Guenet <kevin.guenet@opisto.com>
2025-04-26 23:14:30 +0200

    fix: suppression de l'import RouterOutlet inutilisé dans app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
     templateUrl: 'app.component.html',
     styleUrl: './app.component.css'
 })
