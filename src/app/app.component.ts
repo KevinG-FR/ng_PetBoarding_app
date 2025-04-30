d6723473ddb0a4e6ef7dbeb19e12b10f4aa15781
Kevin Guenet <kevin.guenet@opisto.com>
2025-04-30 23:19:29 +0200

    refactor: migration vers SCSS et ajout des dossiers layout et shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "./layout/navigation/navigation.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: 'app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent],
+  templateUrl: 'app.component.html',
+  styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit{  
+export class AppComponent implements OnInit {
+  private faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
   http = inject(HttpClient);
-  title = 'ng-PetBoarding-app';
+  title = 'ng-PetBoarding-app'; 
   data: any;
 
   ngOnInit(): void {
@@ -18,5 +26,11 @@ export class AppComponent implements OnInit{
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
     })
+
+    this.intiAwesome();
+  }
+
+  private intiAwesome(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
   }
 }
